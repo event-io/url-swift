4453ececcf781ba5d9bbbc01e872e502ac2c8b5f
Luca <corallo.luca@outlook.com>
2023-10-04 22:14:22 +0200

    Creating DTO for handling authentication to supabase

---
+++ src/main/java/io/events/dto/TokenResponseDTO.java
@@ -0,0 +1,29 @@
+package io.events.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenResponseDTO {
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    @JsonProperty("expires_at")
+    private long expiresAt;
+
+    @JsonProperty("refresh_token")
+    private String refreshToken;
+
+}
