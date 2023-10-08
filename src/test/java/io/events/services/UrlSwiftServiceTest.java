cede10c3f3ac87786cd7def18bdf77d3446b6e73
Luca <corallo.luca@outlook.com>
2023-10-08 16:32:33 +0200

    Define URLSwitService test suite

---
+++ src/test/java/io/events/services/UrlSwiftServiceTest.java
@@ -0,0 +1,67 @@
+package io.events.services;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class UrlSwiftServiceTest {
+    
+    @Test
+    public void testGetRedirectURL_shouldReceiveURL() {
+      fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetRedirectURL_shouldNotFound() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetInfo_WithSabouter_shouldHandleError() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetInfo_shouldReceiveInfo() {
+      fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetInfo_shouldNotFound() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetRedirectURL_WithSabouter_shouldHandleError() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateShortenedURL_shouldReceiveURL() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateShortenedURL_shouldBeInputIncorrect() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateShortenedURL_shouldBeAConflict() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateShortenedURL_withB62KGSabouter_shouldHandlerError() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCreateShortenedURL_withSabouter_shouldHandlerError() {
+        fail("Not yet implemented");
+    }
+    
+}
