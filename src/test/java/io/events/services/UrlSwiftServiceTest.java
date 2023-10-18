2079504d991bbd0db921707c7ffd82a42197cfb7
Luca <corallo.luca@outlook.com>
2023-10-18 23:00:59 +0200

    Implementing testGetRedirectURL_shouldReceiveURL

--- src/test/java/io/events/services/UrlSwiftServiceTest.java
+++ src/test/java/io/events/services/UrlSwiftServiceTest.java
@@ -1,17 +1,68 @@
 package io.events.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
 
+import java.util.concurrent.ExecutionException;
+
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.Test;
 
+import io.events.dto.TokenRequestDTO;
+import io.events.mocks.MockSupabaseClient;
+import io.events.mocks.MockSupabaseUrlSwiftClient;
+import io.events.restclients.SupabaseClient;
+import io.events.restclients.SupabaseUrlSwiftClient;
+import io.quarkus.test.InjectMock;
 import io.quarkus.test.junit.QuarkusTest;
-
+import jakarta.inject.Inject;
 @QuarkusTest
 public class UrlSwiftServiceTest {
+
+    @InjectMock
+    @RestClient
+    private SupabaseClient mockSupabaseClient;
+
+    @InjectMock
+    @RestClient
+    private SupabaseUrlSwiftClient mockSupabaseUrlSwiftClient;
+
+    @Inject
+    private UrlSwiftService urlSwiftService;
+
     
     @Test
     public void testGetRedirectURL_shouldReceiveURL() {
-      fail("Not yet implemented");
+        //Mock
+        when(mockSupabaseClient.token(
+            any(),
+            any(String.class),
+            any(TokenRequestDTO.class))
+        ).thenReturn(MockSupabaseClient.mockTokenResponseHappyPath);
+
+        when(mockSupabaseUrlSwiftClient.getByShortened(
+            anyString(),
+            anyString()
+        )).thenReturn(MockSupabaseUrlSwiftClient.mockGetByShortenedResponseHappyPath());
+
+        //Assign
+        String shortenedUrl = "ABCDE01";
+        String correctOriginalUrl = "http://example-mock.com/1";
+
+        //Act
+        String originalUrl = "";
+        try {
+            originalUrl = urlSwiftService.getRedirectURL(shortenedUrl)
+            .subscribeAsCompletionStage().get();
+        } catch (InterruptedException|ExecutionException e) {
+            fail("Should not fail");
+        }
+
+        //Assert
+        assertEquals(correctOriginalUrl, originalUrl, "Should be equal");
     }
 
     @Test
