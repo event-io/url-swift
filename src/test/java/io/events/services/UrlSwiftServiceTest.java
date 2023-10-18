95ba1361fcae177b3dda8be32e6e4e56a6fa13ea
Luca <corallo.luca@outlook.com>
2023-10-19 00:20:25 +0200

    Implementation testGetInfo_shouldReceiveInfo

--- src/test/java/io/events/services/UrlSwiftServiceTest.java
+++ src/test/java/io/events/services/UrlSwiftServiceTest.java
@@ -1,17 +1,22 @@
 package io.events.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.concurrent.ExecutionException;
 
 import org.eclipse.microprofile.rest.client.inject.RestClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import io.events.dto.LinkShorteningResponseDTO;
 import io.events.dto.TokenRequestDTO;
 import io.events.exceptions.RedirectUrlMatchException;
 import io.events.mocks.MockSupabaseClient;
@@ -101,7 +106,31 @@ public class UrlSwiftServiceTest {
 
     @Test
     public void testGetInfo_shouldReceiveInfo() {
-      fail("Not yet implemented");
+        //Mock
+        when(mockSupabaseUrlSwiftClient.getInfoByOriginal(
+            anyString(),
+            anyString()
+        )).thenReturn(MockSupabaseUrlSwiftClient.mockGetByShortenedResponseHappyPath());
+
+        //Assign
+        String happyPathShortenedUrl = "ABCDE01";
+        String happyPathCorrectOriginalUrl = "http://example-mock.com/1";
+        Instant happyCreatedAt = LocalDateTime.of(2023, 10, 19, 0, 0, 0).toInstant(ZoneOffset.UTC);
+
+        //Act
+        LinkShorteningResponseDTO shorteningResponseDTO = null;
+        try {
+            shorteningResponseDTO = urlSwiftService.getInfo(happyPathCorrectOriginalUrl)
+            .subscribeAsCompletionStage().get();
+        } catch (InterruptedException|ExecutionException e) {
+            fail("Should not fail");
+        }
+
+        //Assert
+        assertNotNull(shorteningResponseDTO, "Should not be null");
+        assertEquals(shorteningResponseDTO.getOriginalLink(), happyPathCorrectOriginalUrl, "Should originalLink be equal");
+        assertEquals(shorteningResponseDTO.getShortnedLink(), happyPathShortenedUrl, "Should shortenedLink be equal");
+        assertEquals(shorteningResponseDTO.getCreatedAt(), happyCreatedAt, "Should createdAt be equal");
     }
 
     @Test
