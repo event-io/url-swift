564cdd80b22158102d30fc46dbea157fea4308e7
Luca <corallo.luca@outlook.com>
2023-10-19 00:20:00 +0200

    adding createdAt happy path param in mock url swift client

--- src/test/java/io/events/mocks/MockSupabaseUrlSwiftClient.java
+++ src/test/java/io/events/mocks/MockSupabaseUrlSwiftClient.java
@@ -1,6 +1,8 @@
 package io.events.mocks;
 
 import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import java.util.Collections;
 import java.util.Set;
 import java.util.concurrent.CompletionStage;
@@ -12,13 +14,14 @@ public class MockSupabaseUrlSwiftClient {
 
     public static final String SHORTNED_LINK_HAPPY_PATH = "ABCDE01"; 
     public static final String ORIGINAL_LINK_HAPPY_PATH = "http://example-mock.com/1";
+    public static final Instant CREATED_AT_HAPPY_PATH = LocalDateTime.of(2023, 10, 19, 0, 0, 0).toInstant(ZoneOffset.UTC);
 
     public static CompletionStage<Set<LinkShorteningResponseDTO>> mockGetByShortenedResponseHappyPath() {
         return Uni.createFrom().item(
             Set.of(new LinkShorteningResponseDTO(
                 SHORTNED_LINK_HAPPY_PATH,
                 ORIGINAL_LINK_HAPPY_PATH,
-                Instant.now()
+                CREATED_AT_HAPPY_PATH
             ))
         ).subscribeAsCompletionStage();
     }
