00c329337c4622cd8416b5b852f3e7308d8d9128
Luca <corallo.luca@outlook.com>
2023-11-06 22:07:27 +0100

    creating resource for redirect

--- src/main/java/io/events/resources/UrlSwiftResource.java
+++ src/main/java/io/events/resources/UrlSwiftResource.java
@@ -1,9 +1,14 @@
 package io.events.resources;
 
+import java.net.URI;
+
 import org.jboss.resteasy.reactive.common.NotImplementedYet;
 
 import io.events.dto.LinkShorteningCreationDTO;
+import io.events.exceptions.RedirectUrlMatchException;
+import io.events.services.UrlSwiftService;
 import io.smallrye.mutiny.Uni;
+import jakarta.inject.Inject;
 import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
@@ -12,14 +17,27 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
-
 @Path("/url-swift")
 public class UrlSwiftResource {
+
+    @Inject
+    private UrlSwiftService urlSwiftService;
     
     @GET
     @Path("/r/{shortenedLink}")
     public Uni<Response> redirect(String shortenedLink) {
-        throw new NotImplementedYet();
+        return Uni.createFrom().item(shortenedLink)
+            .map(item -> {
+                if(item.length() != 7 || !item.matches("[a-zA-Z0-9]*")) throw new IllegalArgumentException("Shortened link must be 7 characters and must respect regex [a-zA-Z0-9]*");
+                return item;
+            })
+            .chain(sL -> Uni.createFrom().completionStage(this.urlSwiftService.getRedirectURL("eq."+shortenedLink).subscribeAsCompletionStage()))
+            .map(redirectURL -> Response.seeOther(URI.create(redirectURL)).build())
+            .onFailure().recoverWithItem(failure -> {
+                if(failure instanceof IllegalArgumentException) return Response.status(Response.Status.BAD_REQUEST).entity(failure.getMessage()).build();
+                if(failure instanceof RedirectUrlMatchException) return Response.status(Response.Status.NOT_FOUND).entity(failure.getMessage()).build();
+                return Response.serverError().entity(failure.getMessage()).build();
+        });
     }
 
     @GET
